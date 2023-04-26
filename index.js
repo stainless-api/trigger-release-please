9ab749675bc91e6eb8f3dd7d7a09edd7549673e3
Robert Craigie <robert@craigie.dev>
2023-04-26 18:14:56 +0100

    Always set `releases_created`

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ async function main() {
 
   const apiKey = core.getInput('stainless-api-key');
   if (!apiKey) {
-    // TODO: check if redundant
     throw new Error('Missing stainless-api-key input');
   }
 
@@ -50,6 +49,8 @@ async function main() {
 
   if (data?.releases?.length) {
     core.setOutput('releases_created', true);
+  } else {
+    core.setOutput('releases_created', false);
   }
 }
 
