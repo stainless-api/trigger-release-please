353c50b9a7474129da648a10827afe6e4b55ec8e
Samuel El-Borai <sam@elborai.me>
2023-08-25 19:44:56 +0200

    Reset base url

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const core = require('@actions/core');
 const fetch = require('node-fetch').default;
 
-const BASE_URL = 'https://api-od3waswa0-stainless-api.vercel.app/api';
+const BASE_URL = 'https://api.stainlessapi.com/api';
 
 async function main() {
   const fullRepo = core.getInput('repo');
